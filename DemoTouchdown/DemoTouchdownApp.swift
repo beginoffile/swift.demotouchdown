57dc9e9f75643bbc84a69a7d7dfed9ea4844a556
cesar <cursos.mapatupisaro@gamail.com>
2024-04-10 18:11:57 -0600

    Initial Commit

---
+++ DemoTouchdown/DemoTouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTouchdownApp.swift
+//  DemoTouchdown
+//
+//  Created by Aguid Ramirez Sanchez on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoTouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
